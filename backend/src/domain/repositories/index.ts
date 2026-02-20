79b9157692dc7d9dd6f7e0e7c5ae2b639c02ae31
pauldatcom <paul.bajodek@gmail.com>
2026-02-20 13:30:57 +0100

    feat(backend): add IFilmRepository and domain repository index

--- backend/src/domain/repositories/index.ts
+++ backend/src/domain/repositories/index.ts
@@ -3,6 +3,9 @@
  */
 
 export * from './IFilmRepository.js';
+export * from './IFriendsRepository.js';
+export * from './IMessageRepository.js';
 export * from './IReviewRepository.js';
+export * from './ITmdbClient.js';
 export * from './IUserRepository.js';
 export * from './IWatchlistRepository.js';
