db2e87b964aaafe39d97b7abc7c374789bd2c9fb
pauldatcom <paul.bajodek@gmail.com>
2026-02-20 13:31:38 +0100

    feat(backend): update DrizzleReview and DrizzleWatchlist repositories

--- backend/src/infrastructure/repositories/index.ts
+++ backend/src/infrastructure/repositories/index.ts
@@ -3,6 +3,8 @@
  */
 
 export * from './DrizzleFilmRepository.js';
+export * from './DrizzleFriendsRepository.js';
+export * from './DrizzleMessageRepository.js';
 export * from './DrizzleReviewRepository.js';
 export * from './DrizzleUserRepository.js';
 export * from './DrizzleWatchlistRepository.js';
