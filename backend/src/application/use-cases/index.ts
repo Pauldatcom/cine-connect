904e9be9dcaa87698cdb08545bf81c05ab8336c8
pauldatcom <paul.bajodek@gmail.com>
2026-03-06 10:18:16 +0100

    fix: restore use-cases index from main (do not add on auth branch)

--- backend/src/application/use-cases/index.ts
+++ backend/src/application/use-cases/index.ts
@@ -1,8 +1,6 @@
-export * from './auth/index.js';
-export * from './films/index.js';
-export * from './friends/index.js';
-export * from './messages/index.js';
-export * from './recommendations/index.js';
+/**
+ * Use Cases - Barrel Export
+ */
+
 export * from './reviews/index.js';
-export * from './users/index.js';
 export * from './watchlist/index.js';
